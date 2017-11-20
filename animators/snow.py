e64b2551c8afb4e65cd3b91071ac59eaea5e3abe
Garrett Edwards <grrttedwards@users.noreply.github.com>
2017-11-19 20:11:32 -0500

    Rework probability calculation

--- animators/snow.py
+++ animators/snow.py
@@ -9,10 +9,10 @@ class SnowAnimator:
     frame_size = 32
 
     def __make_row(self):
-        probability = 3
+        probability = 32
         row = [0] * self.frame_size
         for i, pixel in enumerate(row):
-            if random.randint(1, self.frame_size) - probability <= 0:
+            if random.randint(1, probability) == 1:
                 row[i] = 1
         return row
 
@@ -24,24 +24,3 @@ class SnowAnimator:
             frame.rotate(1)
         self.last_frame = frame
         return frame
-
-    def create_image(self, image, tag):
-        """From the map, generate a PNG along with the path overlaid"""
-        output = Image.new('RGB', (32, 32))
-        for y in range(32):
-            for x in range(32):
-                if image[y][x]:
-                    color = (255, 255, 255)
-                else:
-                    color = (0,0,0)
-                output.putpixel((x, y), color)
-        # output.show()
-        # output.save(f'out\out-{tag}.png')
-        # return output
-
-    #
-    # frame = get_frame()
-    # for i in range(60):
-    #     new_frame = get_frame(frame)
-    #     create_image(new_frame, i)
-    #     frame = new_frame
